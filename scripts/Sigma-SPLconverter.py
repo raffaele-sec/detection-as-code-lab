cc0f1129f1b4018d018ea53dc053fff80946963f
Raffaele <raffaele.longo46@outlook.it>
2026-02-01 03:06:35 +0100

    aggiunta exceptions per collection e conversion delle Sigma rule

--- scripts/Sigma-SPLconverter.py
+++ scripts/Sigma-SPLconverter.py
@@ -5,6 +5,7 @@ import requests
 from sigma.collection import SigmaCollection
 from sigma.backends.splunk import SplunkBackend
 from sigma.pipelines.splunk import splunk_windows_pipeline
+from sigma.exceptions import SigmaCollectionError, SigmaConversionError
 
 #per disabilitare warning SSL, perchè la porta 8089 usata per la gestione delle API di splunk 
 #abilita di default HTTPS.
@@ -19,7 +20,7 @@ SPLUNK_TOKEN = os.environ.get("SPLUNK_TOKEN")
 
 if not SPLUNK_HOST:
     print("URL di Splunk non trovato")
-    #permette a git di terminare il programma con un'errore
+    #permette a git di terminare il programma con un errore
     sys.exit(1)
 elif not SPLUNK_TOKEN:
     print("Token di autenticazione Splunk non trovato")
@@ -28,7 +29,7 @@ elif not SPLUNK_TOKEN:
 
 
 
-#Gli argument della funziona verranno presi dalle regole convertite
+#Gli argument della funzione verranno presi dalle regole convertite
 def deploy_rule(name, query, description):
 
     #creo l'url aggiungendo il path dell'API delle ricerche salvate per gli alert di Splunk
@@ -60,7 +61,7 @@ def deploy_rule(name, query, description):
     
     #il dizionario contenente i campi per la creazione dell'alert vengono passati a "data"
     #se la creazione va a buon fine viene restituito lo status code 201
-    #se la rule è già esistente, viene generato lo status code 409a
+    #se la rule è già esistente, viene generato lo status code 409
     post_api = requests.post(url=splunk_url, headers=headers, verify=False, data=payload)
     
 
@@ -70,6 +71,8 @@ def deploy_rule(name, query, description):
     #Se restituisce status code 409 vuol dire che la rule c'è già, quindi fa l'update.
     elif post_api.status_code == 409:
         print("La regola è già presente. Verrà effettuato l'update!")
+        
+        #devo quindi "contattare" l'url con il nome della regola alla fine
         splunk_url_update=f"{splunk_url}/{name}"
         
         #per l'update della regola devo eliminare il campo "name", sennò da errore 409
@@ -90,16 +93,23 @@ def deploy_rule(name, query, description):
         else:
             print("Errore nell'update della regola.")
             sys.exit(1)
+        
+
 
 rules_path="./rules/"
 
 #importo le regole Sigma dal path "rules" massivamente. SigmaColleciton permette la lettura dei file YAML
-collection_rules=SigmaCollection.load_ruleset([rules_path])
+#collection_rules diventa un oggetto che contiene una lista di regola Sigma
+try:
+    collection_rules=SigmaCollection.load_ruleset([rules_path])
 
-#si imposta il backend per la conversione
-backend = SplunkBackend(processing_pipeline=splunk_windows_pipeline())
+    #si imposta il backend per la conversione
+    backend = SplunkBackend(processing_pipeline=splunk_windows_pipeline())
+except SigmaCollectionError: #gestione errori di collection importata da sigma.exceptions
+    print("Errore di lettura delle regole da SigmaCollection")
 
-#la proprietà "rules" della SigmaCollection permette di accedere ai singoli campi della rule Sigma
+
+#la proprietà "rules" di SigmaCollection permette di accedere alle singole regole nella collection e ai singoli campi della rule Sigma
 #Questo serve per estrarre i campi da passare al payload della POST verso l'API di Splunk per creare l'alert
 #questo ciclo for itera nella lista delle regole
 for rule in collection_rules.rules:
@@ -107,8 +117,11 @@ for rule in collection_rules.rules:
     rule_description=rule.description
 
     #converto la singola regola nell'iterazione. backend.convert() accetta la collection, mentre convert_rule() accetta una singola regola.
-    converted_rule = backend.convert_rule(rule)
+    try:
+        converted_rule = backend.convert_rule(rule)
 
-    #invio i campi estratti regola per regola verso splunk tramite la funziona creata
-    #perchè "converted_rule[0]"? perchè l afunzione backend.convert_rule potrebbe restituire più query da un'unica regola SIGMA, quindi ne teniamo 1.
-    deploy_rule(rule_name, converted_rule[0], rule_description)
+        #invio i campi estratti regola per regola verso splunk tramite la funziona creata
+        #perchè "converted_rule[0]"? perchè l afunzione backend.convert_rule potrebbe restituire più query da un'unica regola SIGMA, quindi ne teniamo 1.
+        deploy_rule(rule_name, converted_rule[0], rule_description)
+    except SigmaConversionError: #gestione errori di conversione importata da sigma.exceptions
+        continue
