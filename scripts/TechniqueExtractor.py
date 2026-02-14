7887fba032e42c35d280d5bed6f13d4657cb29af
raffaele-sec <raffaele.longo46@outlook.it>
2026-02-14 14:29:14 +0100

    modifica nome campo

--- scripts/TechniqueExtractor.py
+++ scripts/TechniqueExtractor.py
@@ -9,7 +9,7 @@ from sigma.exceptions import SigmaCollectionError
 import json
 
 rules_path="./rules/"
-json_fields = { #definisco staticamente i campi del layer JSON da passare al MITRE ATT&CK Navigator
+layer_fields = { #definisco staticamente i campi del layer JSON da passare al MITRE ATT&CK Navigator
     "name" : "Windows Lab - Sigma Coverage (v18)",
     "versions" : {
         "attack": "18",
@@ -58,7 +58,7 @@ try:
 
 
         tech_presente = False
-        for techn in json_fields["techniques"]: #aggiunto controllo per vedere se la Technique ID è già presente, in maniera tale da non sovrascrivere nulla sul layer
+        for techn in layer_fields["techniques"]: #aggiunto controllo per vedere se la Technique ID è già presente, in maniera tale da non sovrascrivere nulla sul layer
             if techn["techniqueID"] == technique_id: # SE la "technique_id" estratta dalle rule nel ciclo attuale corrisponde a una "techniqueID" presente nella lista ""techniques": []"
                 techn["comment"]+=f"\n{rule_name}" #aggiunge il commento con il nome della regola, così da avere una technique ID con più rule name nei commenti
                 tech_presente = True
@@ -66,7 +66,7 @@ try:
 
 
         if tech_presente == False:
-            json_fields["techniques"].append( #inserisco nel dizionario i campi estratti dalle Sigma rule, per poter creare poi il JSON layer di copertura delle ruel
+            layer_fields["techniques"].append( #inserisco nel dizionario i campi estratti dalle Sigma rule, per poter creare poi il JSON layer di copertura delle rule
                 {
                 "techniqueID": f"{technique_id}",
                 "score": 1,
@@ -80,7 +80,7 @@ try:
     
 
     with open ("mitre_navigator_layer.json", "w") as file:
-        JSONlayer= json.dump(json_fields, file, indent=4)
+        JSONlayer= json.dump(layer_fields, file, indent=4)
 
 except SigmaCollectionError:
     print("Errore nel caricamento delle Sigma Rule")
